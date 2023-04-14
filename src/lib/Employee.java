243e99bec4757049659d36fc4bd34876a8ef3c85
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2023-04-14 20:10:59 +0700

    menghapus variable employeId
    dikarenakan variable employeId tidak digunakan

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 public class Employee {
 
-	private String employeeId;
 	private String firstName;
 	private String lastName;
 	private String idNumber;
@@ -31,8 +30,7 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
-		this.employeeId = employeeId;
+	public Employee(String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = idNumber;
